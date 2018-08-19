b58619937de4af16242bbc248bf8a3d2aa1e4cf3
dankuck <dankuck@gmail.com>
2018-08-19 17:03:57 -0400

    A good enough view of the game, for now

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -4,8 +4,31 @@
     game {{ $game }}
 
     <div class="row">
-        @for ($i = $game->deck->card_count; $i > 0; $i--)
-            <card card='BACK'></card>
-        @endfor
+        <div class="deck">
+            @for ($i = $game->deck->card_count; $i > 0; $i--)
+                <card card='BACK' style="position: absolute; left: {{ $i * 5 }}px;"></card>
+            @endfor
+        </div>
     </div>
+    <div class="row">
+        <div class="col-6">
+            <h3>Dealer's Hand</h3>
+            @foreach ($game->dealer_hand as $card)
+                <card card="{{ $card }}"></card>
+            @endforeach
+        </div>
+        <div class="col-6">
+            <h3>Player's Hand</h3>
+            @foreach ($game->player_hand as $card)
+                <card card="{{ $card }}"></card>
+            @endforeach
+        </div>
+    </div>
+@endsection
+
+@section('style')
+.deck {
+    position: relative;
+    min-height: 200px;
+}
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,10 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <style>
+            @yield('style');
+        </style>
     </head>
     <body>
         <div class="container">
